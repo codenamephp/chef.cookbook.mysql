0b0c2097c9b9e231ebe6f18c94fc65056c96f8de
Bastian Schwarz <bastian@codename-php.de>
2017-12-19 22:56:10 +0100

    Fixed styling errors with rubocop

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -10,24 +10,24 @@ describe 'codenamephp_mysql::default' do
   context 'When all attributes are default' do
     let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
     let(:template) { chef_run.template('/etc/mysql/conf.d/50-innodb.cnf') }
-    
+
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-    
+
     it 'installs mysql server from package' do
       expect(chef_run).to install_package('mysql-server')
     end
-    
+
     it 'installs mysql client from package' do
       expect(chef_run).to install_package('mysql-client')
     end
-    
+
     it 'makes sure the service is started and enabled' do
       expect(chef_run).to enable_service('mysql')
       expect(chef_run).to start_service('mysql')
     end
-    
+
     it 'adds innodb config template and restarts mysql service immediately' do
       expect(chef_run).to create_template('/etc/mysql/conf.d/50-innodb.cnf').with(
         source: 'innodb.erb',
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,10 +5,10 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-control "mysql-1.0" do
-  title "Install mysql/mariadb"
-  desc "Install mysql/mariadb"
-  
+control 'mysql-1.0' do
+  title 'Install mysql/mariadb'
+  desc 'Install mysql/mariadb'
+
   describe service('mysql') do
     it { should be_installed }
     it { should be_enabled }
@@ -16,14 +16,14 @@ control "mysql-1.0" do
   end
 
   describe command('mysql --version') do
-    its('stdout') { should match /mysql  Ver/ }
+    its('stdout') { should match(/mysql  Ver/) }
   end
-  
+
   describe file('/etc/mysql/conf.d/50-innodb.cnf') do
-   it { should exist }
+    it { should exist }
   end
-  
+
   describe command('mysql -NBe "SHOW GLOBAL VARIABLES WHERE Variable_name=\'innodb_file_per_table\';"') do
-    its('stdout') { should match /innodb_file_per_table\s+ON/ }
+    its('stdout') { should match(/innodb_file_per_table\s+ON/) }
   end
 end
