ced211f68694b65414792526ba50c8fff5275b06
Bastian Schwarz <bastian@codename-php.de>
2017-12-23 19:17:59 +0100

    Removed responds_to from metadate since supermarket does not recognize them otherwise

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache-2.0'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.0.4'
 chef_version '>= 13.0' if respond_to?(:chef_version)
-issues_url 'https://github.com/codenamephp/chef.cookbook.mysql/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/codenamephp/chef.cookbook.mysql' if respond_to?(:source_url)
+issues_url 'https://github.com/codenamephp/chef.cookbook.mysql/issues'
+source_url 'https://github.com/codenamephp/chef.cookbook.mysql'
 
 supports 'debian', '~>9.3'
