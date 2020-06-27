dc8a4af7fd6452f2cd379d6fc44de1229c9d38d6
Bastian Schwarz <bastian@codename-php.de>
2020-06-27 12:27:12 +0000

    Updated build files

--- metadata.rb
+++ metadata.rb
@@ -5,9 +5,8 @@ maintainer 'Bastian Schwarz'
 maintainer_email 'bastian@codename-php.de'
 license 'Apache-2.0'
 description 'A cookbook to install mariadb on debian.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.1'
-chef_version '>= 13.0' if respond_to?(:chef_version)
+chef_version '>= 13.0'
 issues_url 'https://github.com/codenamephp/chef.cookbook.mysql/issues'
 source_url 'https://github.com/codenamephp/chef.cookbook.mysql'
 
