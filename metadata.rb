a32dfa656094ac41788b301ed66983feae2512e7
Bastian Schwarz <bastian@codename-php.de>
2020-06-28 17:30:42 +0200

    Pushed new version with updated debian constraint

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,11 @@ maintainer 'Bastian Schwarz'
 maintainer_email 'bastian@codename-php.de'
 license 'Apache-2.0'
 description 'A cookbook to install mariadb on debian.'
-version '2.0.1'
+version '2.0.2'
 chef_version '>= 13.0'
 issues_url 'https://github.com/codenamephp/chef.cookbook.mysql/issues'
 source_url 'https://github.com/codenamephp/chef.cookbook.mysql'
 
-supports 'debian', '~>10.0'
+supports 'debian'
 
 depends 'apt'
